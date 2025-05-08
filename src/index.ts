bf11f31908194c348a887e17087d91a53806f901
jacobjshelp <jacobjshelp>
2025-05-08 18:08:40 +0400

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import { dummyBoardingPasses } from './data'
+import cors from 'cors'
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT || 5000
+
+app.use(cors())
 
 app.get('/boardingPasses', (req: Request, res: Response) => {
   setTimeout(() => {
